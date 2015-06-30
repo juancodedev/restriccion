c20daec4b8a676f67e3bb9c7f38be93d7c6aae38
burabure <nicolas@santiagolab.cl>
2015-06-30 19:43:34 -0300

    mocks emergencia

--- app/modules/__tests__/scrape-test.js
+++ app/modules/__tests__/scrape-test.js
@@ -12,39 +12,51 @@ describe('scrape', function(){
 
 
   describe('#parseNumerosRestriccion', function(){
-      var mockConSello = ['Martes 30 de Junio: sin sello verde 9-0-1-2 3-4, con sello verde 1-2', 'Preemergencia Ambiental'];
-      var mockSinSello = ['Martes 30 de Junio: sin sello verde 9-0-1-2 3-4, con sello verde sin restriccion', 'Alerta Ambiental'];
+      var mockEmergencia = ['Martes 30 de Junio: sin sello verde 5-6-7-8-9-0-1-2, con sello verde 1-2-3-4', 'Emergencia Ambiental'];
+      var mockPreemergencia = ['Martes 30 de Junio: sin sello verde 9-0-1-2 3-4, con sello verde 1-2', 'Preemergencia Ambiental'];
+      var mockAlerta = ['Martes 30 de Junio: sin sello verde 9-0-1-2 3-4, con sello verde sin restriccion', 'Alerta Ambiental'];
       var parseNumerosRestriccion = scrape.parseNumerosRestriccion;
 
       it('should return an object', function(){
-        parseNumerosRestriccion(mockConSello).should.be.a('object');
-        parseNumerosRestriccion(mockSinSello).should.be.a('object');
+        parseNumerosRestriccion(mockEmergencia).should.be.a('object');
+        parseNumerosRestriccion(mockPreemergencia).should.be.a('object');
+        parseNumerosRestriccion(mockAlerta).should.be.a('object');
       });
 
       it('object should have formated numeros and be an array', function(){
-        parseNumerosRestriccion(mockConSello).should.have.deep.property('numeros.conSello').and.be.a('array');
-        parseNumerosRestriccion(mockSinSello).should.have.deep.property('numeros.conSello').and.be.a('array');
+        parseNumerosRestriccion(mockEmergencia).should.have.deep.property('numeros.conSello').and.be.a('array');
+        parseNumerosRestriccion(mockPreemergencia).should.have.deep.property('numeros.conSello').and.be.a('array');
+        parseNumerosRestriccion(mockAlerta).should.have.deep.property('numeros.conSello').and.be.a('array');
 
-        parseNumerosRestriccion(mockConSello).should.have.deep.property('numeros.sinSello').and.be.a('array');
-        parseNumerosRestriccion(mockSinSello).should.have.deep.property('numeros.sinSello').and.be.a('array');
+        parseNumerosRestriccion(mockEmergencia).should.have.deep.property('numeros.sinSello').and.be.a('array');
+        parseNumerosRestriccion(mockPreemergencia).should.have.deep.property('numeros.sinSello').and.be.a('array');
+        parseNumerosRestriccion(mockAlerta).should.have.deep.property('numeros.sinSello').and.be.a('array');
       });
 
       it('object should have proper fecha type', function(){
-        parseNumerosRestriccion(mockConSello).should
+        parseNumerosRestriccion(mockEmergencia).should
                 .have.property('fecha')
                 .and.be.a('date'); // TODO: revisar el generador de dates, deberia devolver false si no hay fecha
 
-        parseNumerosRestriccion(mockSinSello).should
+        parseNumerosRestriccion(mockPreemergencia).should
+                .have.property('fecha')
+                .and.be.a('date');
+
+        parseNumerosRestriccion(mockAlerta).should
                 .have.property('fecha')
                 .and.be.a('date');
       });
 
       it('object should have proper estatus type', function(){
-        parseNumerosRestriccion(mockConSello).should
+        parseNumerosRestriccion(mockEmergencia).should
                 .have.property('estatus')
                 .and.to.match(/ambiental/i);
 
-        parseNumerosRestriccion(mockSinSello).should
+        parseNumerosRestriccion(mockPreemergencia).should
+                .have.property('estatus')
+                .and.to.match(/ambiental/i);
+
+        parseNumerosRestriccion(mockAlerta).should
                 .have.property('estatus')
                 .and.to.match(/ambiental/i);
       });
