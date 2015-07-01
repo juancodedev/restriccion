7f46de8faa90edf5b45f251ec9bc6769a3f61b72
burabure <nicolas@santiagolab.cl>
2015-07-01 13:04:24 -0300

    RestrictionDay Docs + addId

--- app/models/RestrictionDay.js
+++ app/models/RestrictionDay.js
@@ -1,5 +1,20 @@
-import mongoose from 'mongoose';
+/**
+ * @module RestrictionDay
+ * @description a data model representing RestrictionDay
+ */
 
+import mongoose from 'mongoose';
+import moment from 'moment';
+
+/**
+ * RestrictionDay's Schema
+ * @param {string} _id
+ * @param {date} fecha
+ * @param {string} estatus
+ * @param {object} numeros
+ * @param {array|boolean} numeros.sinSello
+ * @param {array|boolean} numeros.conSello
+ */
 const Schema = mongoose.Schema({
   _id    : String,
   fecha  : Date,
@@ -9,24 +24,42 @@ const Schema = mongoose.Schema({
     conSello: Array
   }
 });
-
 const RestrictionDay = mongoose.model('RestrictionDay', Schema);
 
-export function set(obj) {
-  const document = new RestrictionDay(obj);
-  const query = {'_id': obj._id};
 
-  RestrictionDay.findOneAndUpdate(query, document, {upsert: true}, function(err, doc){
-    if (err) { return console.error(err); }
-    console.log('Saved! : ', doc || document);
-  });
+/**
+ * Creates or Updates a RestrictionDay record
+ * @param {object} restrictionDayData an object according to RestrictionDay's Schema
+ */
+export function set(restrictionDayData) {
+  const document = new RestrictionDay( addId(restrictionDayData) );
+  const query    = {'_id': restrictionDayData._id};
+
+  RestrictionDay
+    .findOneAndUpdate(query, document, {upsert: true}, function(err, doc){
+      if (err) { return console.error(err); }
+      console.log('Saved! : ', doc || document);
+    });
 }
 
-export function create(obj) {
-  const document = new RestrictionDay(obj);
+/**
+ * Creates a RestrictionDay record
+ * @param {object} restrictionDayData an object according to RestrictionDay's Schema
+ */
+export function create(restrictionDayData) {
+  const document = new RestrictionDay( addId(restrictionDayData) );
 
   document.save(function (err, doc) {
     if (err) { return console.error(err); }
     console.log('Saved! : ', doc);
   });
+}
+
+/**
+ * calulates and adds _id from restrictionDayData.fecha
+ * @param {object} restrictionDayData an object according to RestrictionDay's Schema
+ * @return {object}
+ */
+function addId(restrictionDayData) {
+  return Object.assign(restrictionDayData, {_id: moment(restrictionDayData.fecha).format('YYYY-MM-DD')});
 }
--- devEntry.js
+++ devEntry.js
@@ -1,19 +1,7 @@
 require("babel/register");
 require('./app/modules/connectToDB.js');
-const moment = require('moment');
+const scrape = require('./app/modules/scrape.js');
 const RestrictionDay = require('./app/models/RestrictionDay.js');
 
-RestrictionDay.set({
-  _id    : "12-10-1925",
-  fecha  : moment("10-10-1925", "DD-MM-YYYY"),
-  estatus: "Ambiente Nuclear",
-  numeros: {
-    sinSello: [1, 2, 3, 4, 5],
-    conSello: false
-  }
-});
-
-/*
 scrape.fetchNumerosRestriccion()
-  .then(function(x) { console.log(x); });
-*/
+  .then(function(x) { RestrictionDay.set(x); });
