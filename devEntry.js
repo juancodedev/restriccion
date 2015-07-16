419a90ad4a545ec159dcc4e6e961da3d2d37600b
Federico Hernández <federicohernandez.ve@gmail.com>
2015-07-16 02:23:01 -0400

    Testing kue to send emails

--- devEntry.js
+++ devEntry.js
@@ -2,7 +2,7 @@ require('./app/modules/connectToDB.js');
 const scrape = require('./app/modules/scrape.js');
 const RestrictionDay = require('./app/models/RestrictionDay.js');
 const User = require('./app/models/User.js');
-//import {sendEmail} from './app/modules/mailSender';
+import {addEmailToQueue} from './app/modules/mailSender';
 
 scrape.fetchNumerosRestriccion()
   .then(function(datosRestriccion) {
@@ -15,3 +15,32 @@ scrape.fetchNumerosRestriccion()
   })
   .then(function(data) { console.log(data); })
   .catch(function(err) { console.error(err); });
+
+
+var emails = [{
+    'email': 'slabsuno@mailinator.com',
+    'name' : 'slabs',
+    'type' : 'to'
+  },
+  {
+    'email': 'slabsdos@mailinator.com',
+    'name' : 'slabs',
+    'type' : 'to'
+  },
+  {
+    'email': 'slabstres@mailinator.com',
+    'name' : 'slabs',
+    'type' : 'to'
+  }
+];
+
+emails.forEach(em => {
+  var email = [em];
+  addEmailToQueue(email)
+  .then(function(){
+    console.log('FUNCIONO EL ENVÍO');
+  })
+  .catch(function(){
+    console.log('ERROR EN EL ENVÍO');
+  });
+});
