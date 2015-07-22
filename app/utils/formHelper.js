7938956b99a6744e9db6c533e19cc7d6c275e69a
burabure <nicolas@santiagolab.cl>
2015-07-22 16:05:07 -0300

    fix eq ramda

--- app/utils/formHelper.js
+++ app/utils/formHelper.js
@@ -1,8 +1,8 @@
-import {compose, all, eq, values, both, isEmpty, not, isNil, ifElse, always} from 'ramda';
+import {compose, all, equal, values, both, isEmpty, not, isNil, ifElse, always} from 'ramda';
 
-export const allTrue = compose( all(eq(true)), values );
+export const allTrue = compose( all(equal(true)), values );
 const exists = both(compose(not, isNil), compose(not, isEmpty));
 const objExists = compose(exists, values);
 export const allValid = both(objExists, allTrue );
-export const getValidOrInvalid = ifElse( eq(true), always('valid'), always('invalid') );
+export const getValidOrInvalid = ifElse( equal(true), always('valid'), always('invalid') );
 export const getValidatorClass = ifElse( exists, getValidOrInvalid, always(null) );
