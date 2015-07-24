0f1f217f10d9cb1c668d1c1f94a6ca391898b8f6
burabure <nicolas@santiagolab.cl>
2015-07-24 12:51:31 -0300

    mrensaje subscripcion

--- app/components/Subscribe.jsx
+++ app/components/Subscribe.jsx
@@ -30,7 +30,7 @@ export default class Subscribe extends React.Component {
 
     return (
       <section className="subscripcion">
-        <h4 className="white-text">Te notificamos cuanto estes en restricción</h4>
+        <h4 className="white-text">¿Te avisamos cuanto tengas restricción?</h4>
         <form action="">
           <div className="row">
              <div className="input-field select col s12 m7">
