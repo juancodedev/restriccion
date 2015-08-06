af3c187f0b7578fb42a15792bda225e2f2708e30
burabure <nicolas@santiagolab.cl>
2015-08-06 14:23:39 -0300

    fix CSP schemes...

--- server.js
+++ server.js
@@ -34,10 +34,11 @@ if(__PRODUCTION__) {
   app.use(helmet.contentSecurityPolicy({
     defaultSrc: ["'self'"],
     scriptSrc : ["'self'", "'unsafe-inline'", "'unsafe-eval'",
-                 '*.google-analytics.com:*', '*.cloudflare.com:*'],
-    styleSrc: ["'self'", "'unsafe-inline'", '*.cloudflare.com:*'],
-    imgSrc  : ["'self'", '*.google-analytics.com:*'],
-    fontSrc : ["'self'", '*.cloudflare.com:*']
+                 'https://*.google-analytics.com', 'http://*.google-analytics.com',
+                 'https://*.cloudflare.com', 'http://*.cloudflare.com'],
+    styleSrc: ["'self'", "'unsafe-inline'", 'https://*.cloudflare.com', 'http://*.cloudflare.com'],
+    imgSrc  : ["'self'", 'https://*.google-analytics.com', 'http://*.google-analytics.com'],
+    fontSrc : ["'self'", 'https://*.cloudflare.com', 'http://*.cloudflare.com']
   }));
 }
 
