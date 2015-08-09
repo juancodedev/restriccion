765c0e3167ec97ef340d071343604ae47049ec87
burabure <nicolas@santiagolab.cl>
2015-08-09 04:53:18 -0300

    drop docker stuff

--- app/config/db.js
+++ app/config/db.js
@@ -3,7 +3,7 @@ import {__ENVIRONMENT__} from '../config/envs';
 const db = {
   test       : {db: 'mongodb://localhost/tengo-restriccion-test'},
   development: {db: 'mongodb://localhost/tengo-restriccion-dev'},
-  production : {db: 'mongodb://mongodb/tengo-restriccion'}
+  production : {db: 'mongodb://localhost/tengo-restriccion'}
 };
 
 export default db[__ENVIRONMENT__];
--- app/config/redis.js
+++ app/config/redis.js
@@ -3,7 +3,7 @@ import {__ENVIRONMENT__} from '../config/envs';
 const redis = {
   test       : {host: '127.0.0.1'},
   development: {host: '127.0.0.1'},
-  production : {host: 'redis'}
+  production : {host: '127.0.0.1'}
 };
 
 export default redis[__ENVIRONMENT__];
