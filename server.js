ec8689cf6413b1d7f658d03c8b135494b47c2e24
burabure <nicolas@santiagolab.cl>
2015-08-13 17:38:13 -0300

    add facebook csp

--- server.js
+++ server.js
@@ -35,8 +35,9 @@ if(__PRODUCTION__) {
   app.use(helmet.contentSecurityPolicy({
     defaultSrc: ["'self'"],
     scriptSrc : ["'self'", "'unsafe-inline'", "'unsafe-eval'",
-                 'https://*.google-analytics.com', 'http://*.google-analytics.com',
-                 'https://*.cloudflare.com', 'http://*.cloudflare.com'],
+                'http://*.facebook.net/', 'https://*.facebook.net/',
+                'https://*.google-analytics.com', 'http://*.google-analytics.com',
+                'https://*.cloudflare.com', 'http://*.cloudflare.com'],
     styleSrc: ["'self'", "'unsafe-inline'", 'https://*.cloudflare.com', 'http://*.cloudflare.com'],
     imgSrc  : ["'self'", 'https://*.google-analytics.com', 'http://*.google-analytics.com'],
     fontSrc : ["'self'", 'https://*.cloudflare.com', 'http://*.cloudflare.com']
