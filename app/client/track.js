7f7c3cc7118e6c420ee88b0dc8b056a25a497973
burabure <nicolas@santiagolab.cl>
2015-08-14 22:08:18 -0300

    fix ga tracking

--- app/client/track.js
+++ app/client/track.js
@@ -2,5 +2,5 @@ const __FB_TRACK_ID__ = 6029316041000;
 
 export function trackSubscription(userEmail) {
   window._fbq.push(['track', __FB_TRACK_ID__, {'value': '0.00', 'currency': 'CLP'}]);
-  window.ga('send', 'event', 'Account', 'Subscribed', 'email', userEmail);
+  window.ga('send', 'event', 'Account', 'Subscribed', userEmail);
 }
