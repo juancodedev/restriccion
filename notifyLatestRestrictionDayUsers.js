5b075e00016577953c1c55a2564295cf93f46d5d
BuraBure <nicolas@santiagolab.cl>
2015-08-31 23:14:09 -0300

    manual notificacion script

---
+++ notifyLatestRestrictionDayUsers.js
@@ -0,0 +1,28 @@
+/*
+ * This Script notifies restricted users for the latest restrictionDayData in the DB
+ */
+
+// Lets use babel for ESNext features
+require('babel/register');
+
+// Connect to App DB
+require('./app/modules/connectToDB');
+
+// Requires
+const envs = require('./app/config/envs');
+const __PRODUCTION__ = envs.__PRODUCTION__;
+const __DEVELOPMENT__ = envs.__DEVELOPMENT__;
+const RestrictionDay = require('./app/models/RestrictionDay');
+const Users = require('./app/models/User');
+const job = require('./app/jobs/notifyRestrictedUsersJob');
+
+// Main
+RestrictionDay.getLatest()
+  .then( function(restrictionDayData) {
+    console.log(restrictionDayData);
+    return User.allWithRestriction(restrictionDayData.numeros);
+  })
+  .then( function(users) {
+    console.log(users);
+    //job.addNotifyRestrictedUsersJob(users, restrictionDayData);
+  });
